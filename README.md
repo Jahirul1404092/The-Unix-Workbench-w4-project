ac779f69c787727cbdef3ebef159f92e5e8f1571
Jahirul1404092 <alensamsa3@gmail.com>
2022-02-07 20:27:11 +0600

    codes are updated

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # The-Unix-Workbench-w4-project
-date and time = Mon Feb  7 18:33:17 BST 2022
-Total number of line in guessinggame.sh is 22
+date and time = Mon Feb  7 20:25:09 BST 2022
+Total number of line in guessinggame.sh is 30
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 function guessinggame {
 
 number_of_files=$(ls | wc -l)
@@ -6,17 +8,23 @@ while [[ true ]]
 do
   echo "how many files are in the current directory?"
   read var
-  if [[ $var -eq $number_of_files ]]
+  if [[ $var -gt 0 ]]
     then
-      echo "congratulations!! current directory has $var files"
-      break
-  elif [[ $var -gt $number_of_files ]]
-    then
-      echo "Too high"
+      if [[ $var -eq $number_of_files ]]
+        then
+          echo "congratulations!! current directory has $var files"
+          break
+      elif [[ $var -gt $number_of_files ]]
+        then
+          echo "Too high"
+      else
+        echo "Too low"
+      fi
   else
-    echo "Too low"
+    echo "Enter positive Integer value"
   fi
 done
 }
 
 guessinggame
+
